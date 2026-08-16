e3c05223d9e91bb528605d1dee42b21c4b1d2187
Danil Tremazov <tremazovd@bk.ru>
2026-08-16 15:32:46 +0700

    final version

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CPP2025/lab7
-# Build directory: C:/CPP2025/lab7/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
